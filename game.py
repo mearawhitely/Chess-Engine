5b4f441692bfa3365204541dfd6af58140e92f76
mearawhitely <mearawhitely04@gmail.com>
2025-11-28 21:25:55 -0500

    added framework for Game class and Piece class, as well as added text box for user to enter their name

---
+++ game.py
@@ -0,0 +1,4 @@
+class Game:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
---
+++ pieces.py
@@ -0,0 +1,99 @@
+class Piece:
+    def __init__(self, x, y, color, image, type, rank):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.image = image
+        self.type = type
+
+    def MovePiece(self, x, y):
+        self.x = x
+        self.y = y
+
+    def GetPositionX(self):
+        return self.x
+    
+    def GetPositionY(self):
+        return self.y
+    
+    def GetColor(self):
+        return self.color
+    
+    def GetImage(self):
+        return self.image
+    
+    def GetType(self):
+        return self.type
+
+    def GetRank(self):
+        return self.rank
+    
+    def GetPossibleMoves(self):
+        if type == "Pawn":
+            possible_moves = []
+        elif type == "Bishop[]":
+            possible_moves = []
+        elif type == "Rook[]":
+            possible_moves = []
+        elif type == "Knight[]":
+            possible_moves = []
+        elif type == "Queen[]":
+            possible_moves = []
+        elif type == "King[]":
+            possible_moves = []
+        else:
+            possible_moves = None
+    
+
+
+class Pawn(Piece):
+    type = "Pawn"
+    rank = 2
+
+    def __init__(self, x, y, color, image):
+        super().__init__(x, y, color, image, self.type, self.rank)
+
+
+
+class Bishop(Piece):
+    type = "Bishop"
+    rank = 3
+
+    def __init__(self, x, y, color, image):
+        super().__init__(x, y, color, image, self.type, self.rank)
+
+
+
+class Rook(Piece):
+    type = "Rook"
+    rank = 3
+
+    def __init__(self, x, y, color, image):
+        super().__init__(x, y, color, image, self.type, self.rank)
+
+
+
+class Knight(Piece):
+    type = "Knight"
+    rank = 4
+
+    def __init__(self, x, y, color, image):
+        super().__init__(x, y, color, image, self.type, self.rank)
+
+
+
+class Queen(Piece):
+    type = "Queen"
+    rank = 3
+
+    def __init__(self, x, y, color, image):
+        super().__init__(x, y, color, image, self.type, self.rank)
+
+
+
+class King(Piece):
+    type = "King"
+    rank = 10
+
+    def __init__(self, x, y, color, image):
+        super().__init__(x, y, color, image, self.type, self.rank)
